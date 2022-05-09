8ae80167a7096f77df04385f3791a9f084cefa94
정서연 <jjungseoyeon@naver.com>
2022-05-09 17:12:54 +0900

    Add home form and layout

--- src/main/java/com/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/com/jpabook/jpashop/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.jpabook.jpashop.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @Slf4j
 public class HomeController {
